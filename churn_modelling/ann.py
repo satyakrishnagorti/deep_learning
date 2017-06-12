413c962312b7d9bdaab69720b7ce0b4ebbbaed93
Satya Krishna Gorti <satyakrishna.gorti@gmail.com>
2017-06-13 01:02:45 +0530

    added dropout and parameter tuning

--- churn_modelling/ann.py
+++ churn_modelling/ann.py
@@ -1,5 +1,8 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -48,28 +51,34 @@ def ttsplit(X, y):
     return X_train, X_test, y_train, y_test
 
 
-def build_classifier():
+def build_classifier(optimizer):
 
     # Network dim-> (11, 6, 6, 1)
     classifier = Sequential()
     classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_dim=11))
+    #classifier.add(Dropout(p=0.1))
     classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+    #classifier.add(Dropout(p=0.1))
     classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
-    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    classifier.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
     return classifier
 
+# parameter tuning
+def tune_classfier(classifier, X_train, y_train):
+    parameters = {'batch_size': [25, 32],
+                  'epochs': [50],
+                  'optimizer': ['adam', 'rmsprop']}
+    grid_search = GridSearchCV(estimator=classifier, param_grid=parameters, scoring='accuracy', cv=10) # cv => cross validation (10 sets)
+    grid_search = grid_search.fit(X_train, y_train)
+    best_parameters = grid_search.best_params_
+    best_accuracy = grid_search.best_score_
 
-def main():
-    X, y = read_data_set("Churn_Modelling.csv")
-    X, y = tranform_data(X, y)
-    X_train, X_test, y_train, y_test = ttsplit(X, y)
-    X_train, X_test = feature_scale(X_train, X_test)
-    
-    classifier = build_classifier()
+    return best_parameters, best_accuracy
 
-    classifier.fit(X_train, y_train, batch_size=10, epochs=50)
 
+
+def predict_classes(classifier, X_test, y_test):
     y_pred = classifier.predict(X_test)
     y_pred = (y_pred > 0.5)
 
@@ -83,6 +92,26 @@ def main():
 
     Accuracy: ~ 86%
     """
+
+
+def main():
+    X, y = read_data_set("Churn_Modelling.csv")
+    X, y = tranform_data(X, y)
+    X_train, X_test, y_train, y_test = ttsplit(X, y)
+    X_train, X_test = feature_scale(X_train, X_test)
+
+    classifier = KerasClassifier(build_fn=build_classifier)
+    best_parameters, best_accuracy = tune_classfier(classifier, X_train, y_train)
+
+    print(best_parameters, best_accuracy)
+    """ {'batch_size': 32, 'optimizer': 'rmsprop', 'epochs': 50} acc: 0.8385 """
+
+    classifier = build_classifier('rmsprop')
+
+    classifier.fit(X_train, y_train, batch_size=32, epochs=50)
+
+    predict_classes(classifier, X_test, y_test)
+
 
 
 if __name__ == '__main__':
